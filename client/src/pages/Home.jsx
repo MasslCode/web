04b4bf48a042fee6787d3b167bedc7de7eb9087c
MasslCode <marcelgailberger.code@gmail.com>
2025-01-17 13:39:52 +0100

    add missing prop

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -56,7 +56,7 @@ export default function Homepage()
             ) : (
                 <div></div>
             )}
-            <Albumdisplay albums={albums}/>
+            <Albumdisplay albums={albums} loading={loading}/>
             </div>
             <Pagination 
                 count={totalPages}
