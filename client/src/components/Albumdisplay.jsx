fedf7666fe0ce383f620958479751e001b529a17
MasslCode <marcelgailberger.code@gmail.com>
2025-02-16 19:27:15 +0100

    display flex

--- client/src/components/Albumdisplay.jsx
+++ client/src/components/Albumdisplay.jsx
@@ -9,7 +9,7 @@ export default function Albumdisplay({albums, loading, currentPage })
 
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 4 }}>
-            <AlbumsSort sx={{display: 'left'}}></AlbumsSort>
+            <AlbumsSort sx={{display: 'flex'}}></AlbumsSort>
             <Grid container spacing={4} justifyContent="center" >
                 {loading && albums.length === 0 ? cardSkeletons.map((_, index) => (
                     <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
