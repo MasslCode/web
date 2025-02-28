0345b158cf1056abd98285297c2bfd38ad58b5d0
MasslCode <marcelgailberger.code@gmail.com>
2025-02-28 21:53:26 +0100

    add color scheme to button

--- client/src/components/Editbutton.jsx
+++ client/src/components/Editbutton.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable react/prop-types */
 import { Button } from "@mui/material";
+import { Edit } from "@mui/icons-material";
+import { createTheme } from '@mui/material/styles';
 
 export default function Editbutton({album})
 {
+    const theme = createTheme({
+        palette: {
+          babyblue: '#77a9bf',
+        },
+      });
+
     const handleClick = () => {
         console.log("album clicked: ", album);
     };
@@ -10,7 +18,10 @@ export default function Editbutton({album})
     return(
         <Button
             onClick={handleClick}
+            color="babyblue"
+            endIcon={<Edit />}
         >
+        Edit 
         </Button>
     );
 }
