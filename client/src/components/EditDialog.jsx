543c4ddc224bda566cf41ce8c1a2f45ad5fec7d3
MasslCode <marcelgailberger.code@gmail.com>
2025-05-09 15:57:58 +0200

    add open up logic

--- client/src/components/EditDialog.jsx
+++ client/src/components/EditDialog.jsx
@@ -1,12 +1,32 @@
 /* eslint-disable react/prop-types */
-import { Dialog, DialogActions, Button } from "@mui/material";
+import { Dialog, DialogActions, DialogTitle, Button } from "@mui/material";
 export default function EditDialog({open, close, success, album, songs})
 {
     return (
-        <Dialog>
+        <Dialog
+            open={open}
+            close={close}
+            sx={{ 
+                '& .MuiDialog-paper': {
+                    width: 'auto',
+                    maxWidth: '90vw',
+                    maxHeight: '90vh',
+                    overflow: 'hidden',
+                }
+            }}>
+            <DialogTitle
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                    padding: 2,
+                    fontSize: '1.5rem',
+                }}>
+             Edit album
+            </DialogTitle>
             <DialogActions>
                 <Button onClick={close}> Cancel </Button>
-                <Button variant="contained" onClick={null}> Save </Button>
+                <Button variant="contained" onClick={success}> Save </Button>
             </DialogActions>
         </Dialog>
     );
--- client/src/components/Editbutton.jsx
+++ client/src/components/Editbutton.jsx
@@ -1,15 +1,32 @@
 /* eslint-disable react/prop-types */
 import { Button } from "@mui/material";
 import { Edit } from "@mui/icons-material";
+import EditDialog from "./EditDialog";
+import { useState, useEffect } from "react";
 
 export default function Editbutton({album})
 {
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [selectedAlbum, setSelectedAlbum] = useState(null);
+
     const handleClick = (e) => {
         e.stopPropagation();
         console.log("album clicked: ", album);
+        setSelectedAlbum(album);
+        setDialogOpen(true);
     };
+
+    const handleDialogClose = () => {
+        setDialogOpen(false);
+      };
+
+    useEffect(() => {
+
+
+    },[]);
     
     return(
+        <div>
         <Button
             onClick={ (e) => handleClick(e)}
             endIcon={<Edit />}
@@ -23,5 +40,12 @@ export default function Editbutton({album})
         >
         Edit 
         </Button>
+        <EditDialog 
+            open={dialogOpen}
+            album={selectedAlbum}
+            close={handleDialogClose}
+            TransitionProps={{ onExited: () => setSelectedAlbum(null) }}
+        />
+        </div>
     );
 }
