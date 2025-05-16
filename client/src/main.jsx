5629e2ebbcb880d75be016eaff1737878a535dcc
MasslCode <marcelgailberger.code@gmail.com>
2025-05-16 13:20:05 +0200

    add css file for slider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
- /*to start server, run "npm run dev" in client folder*/
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/index.css'
+import './assets/components.css'
 import Home from './pages/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
